e5d280c3f0b5c3aa7c0d814e2dcb24e9f953e47e
Avain <sports113499@gmail.com>
2024-08-25 23:59:11 +0800

    feat: add base setting(container.grid.color) of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-yellow': '#FFD370',
+      }
+    },
+    container:{
+      screens: {
+        'md': '768px',
+        'lg': '1024px'
+      },
+      center: true,
+      padding: '32px'
+    },
+    screens: {
+      'md': '768px',
+      'lg': '1024px'
+    },
   },
   plugins: [],
 }
-
