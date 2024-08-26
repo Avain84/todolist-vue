abe04e3ae6d65e4d3d21f91330be9a3d068b5685
Avain <sports113499@gmail.com>
2024-08-26 20:59:28 +0800

    feat: add guest page outlet

--- src/components/TodoOutlet.vue
+++ src/components/TodoOutlet.vue
@@ -1,7 +1,14 @@
 <script setup>
-
+import guestImgWeb from '@/assets/guestImgWeb.svg'
 </script>
 
 <template>
-  
+  <div class="container layout-grid">
+    <div class="col-start-2 col-span-5">
+      <img :src="guestImgWeb" alt="To-do List Web">
+    </div>
+    <div class="col-start-8 col-span-4">
+      <h1>Todo</h1>
+    </div>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,21 @@ export default {
     extend: {
       colors: {
         'main-yellow': '#FFD370',
+        'main-black': '#333333',
+        'main-alert': '#D87355',
       }
     },
     container:{
       screens: {
-        'md': '768px',
-        'lg': '1024px'
+        'md': '520px',
+        'lg': '992px'
       },
       center: true,
       padding: '32px'
     },
     screens: {
-      'md': '768px',
-      'lg': '1024px'
+      'md': '520px',
+      'lg': '992px'
     },
   },
   plugins: [],
