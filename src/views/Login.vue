92d02af0113ec183667a3a71064a12b4027f59d3
Avain <sports113499@gmail.com>
2024-09-02 21:20:46 +0800

    feat: add rules. i18n about vee-validate

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,6 +6,17 @@ import TodoOutlet from '@/components/TodoOutlet.vue';
   <TodoOutlet>
     <main>
       <h2 class="font-bold text-2xl text-center">最實用的線上代辦事項服務</h2>
+      <form novalidate>
+        <div>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label for="password">密碼</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit">送出</button>
+      </form>
     </main>
   </TodoOutlet>
 </template>
