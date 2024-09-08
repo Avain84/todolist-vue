0147a8ae98d8fdd9e9f215cb11e5c037d3d5962d
Avain <sports113499@gmail.com>
2024-09-08 20:16:18 +0800

    feat: add global vee-validate import and set login form in LoginPage.vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,27 @@ import '@/index.css'
 import App from './App.vue'
 import router from './router'
 
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
+import { all as AllRules } from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule]);
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
+
+setLocale('zh_TW');
+
 const app = createApp(App)
 
 app.use(router)
+app.component('VForm', Form)
+app.component('VField', Field)
+app.component('ErrorMessage', ErrorMessage)
 
 app.mount('#app')
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,39 +1,8 @@
 <script setup>
-import { defineRule, Form, Field, ErrorMessage, configure } from 'vee-validate';
-import { all } from '@vee-validate/rules';
-import { localize, setLocale } from '@vee-validate/i18n';
-import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
-
 import TodoOutlet from '@/components/TodoOutlet.vue';
-import { ref } from 'vue';
 
-setLocale('zh_TW'); //設置 繁體中文
-configure({
-  generateMessage: localize({ zh_TW: zhTW }), //將訊息轉成繁體
-  validateOnInput: true, //為true時，輸入便會驗證，false離開focus狀態才會驗證
-})
-
-Object.entries(all).forEach(([name, rule]) => {
-  defineRule(name, rule)
-})
-
-//針對欄位 配置 驗證api
-const schema = {
-  email: 'required|email', //規則為 必填，為email格式 ( 規則api參考上方 驗證規則rule文件 )
-  password: 'required|min:6' //規則為 必填，最小要6個字元
-}
-
-const userLoginData = ref({
-  email: '',
-  password: ''
-})
-
-const handleLogin = (validate, resetForm) => {
-  const { valid } = validate();
-  if (valid) {
-    alert('登入成功');
-    resetForm();
-  }
+const handleLogin = (value) => {
+  console.log(value);
 }
 
 </script>
@@ -42,21 +11,22 @@ const handleLogin = (validate, resetForm) => {
   <TodoOutlet>
     <main>
       <h2 class="font-bold text-2xl text-center">最實用的線上代辦事項服務</h2>
-      <Form :validation-schema="schema" v-slot="{ resetForm, validate }">
+      <VForm @submit="handleLogin" v-slot="{ meta }">
         <div>
           <label for="email">Email</label>
-          <Field type="email" name="email" id="email" v-model="userLoginData.email" />
+          <VField type="email" id="email" name="email"
+            rules="required|email" />
           <ErrorMessage name="email" />
+
         </div>
         <div>
-          <label for="password">密碼</label>
-          <Field type="password" name="password" id="password" v-model="userLoginData.password" />
+          <label for="password">Password</label>
+          <VField type="password" id="password" name="password"
+            rules="required|min:6" />
           <ErrorMessage name="password" />
         </div>
-        <button type="submit"
-          @click.prevent="handleLogin(validate, resetForm)"
-        >送出</button>
-      </Form>
+        <button type="submit" :disabled="!meta.valid">登入</button>
+      </VForm>
     </main>
   </TodoOutlet>
 </template>
