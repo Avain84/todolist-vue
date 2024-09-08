4e333a5ddc87cd883b5b57cfa57d0e2837b5b233
Avain <sports113499@gmail.com>
2024-09-08 20:53:07 +0800

    feat: add base field and buttons in sign-up page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/LoginPage.vue'),
+      path: "/",
+      name: "home",
+      component: () => import("@/views/LoginPage.vue"),
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ]
-})
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("@/views/SignUp.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/SignUp.vue
@@ -0,0 +1,37 @@
+<script setup>
+import TodoOutlet from '@/components/TodoOutlet.vue';
+import { ErrorMessage } from 'vee-validate';
+
+</script>
+
+<template>
+  <TodoOutlet>
+    <main>
+      <h2 class="font-bold text-2xl text-center">註冊帳號</h2>
+      <VForm>
+        <div>
+          <label for="email">Email</label>
+          <VField type="email" name="email" id="email" placeholder="請輸入 Email" />
+          <ErrorMessage name="email" />
+        </div>
+        <div>
+          <label for="userName">你的暱稱</label>
+          <VField type="text" name="userName" id="userName" placeholder="請輸入您的暱稱" />
+          <ErrorMessage name="userName" />
+        </div>
+        <div>
+          <label for="password">密碼</label>
+          <VField type="password" name="password" id="password" placeholder="請輸入密碼" />
+          <ErrorMessage name="password" />
+        </div>
+        <div>
+          <label for="password-valid">再次輸入密碼</label>
+          <VField type="password" name="password-valid" id="password-valid" placeholder="請再次輸入密碼" />
+          <ErrorMessage name="password-valid" />
+        </div>
+        <button type="submit">註冊帳號</button>
+        <RouterLink to="/">登入</RouterLink>
+      </VForm>
+    </main>
+  </TodoOutlet>
+</template>
