74e392c602bfc61dc04def00e96ca75540b84d0d
Avain <sports113499@gmail.com>
2024-09-08 20:26:16 +0800

    fix: modify component in router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/LoginPagePage.vue'),
+      component: () => import('@/views/LoginPage.vue'),
     },
     // {
     //   path: '/about',
